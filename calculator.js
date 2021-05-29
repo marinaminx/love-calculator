9ae55545ac48c8780e5c9a68e08c6ac90d90f830
Marina Mihaljevic <marina.mihaljevic95@gmail.com>
2021-05-29 20:06:07 +0200

    created initial js

---
+++ calculator.js
@@ -0,0 +1,12 @@
+const calculating = 'Kalkuliram ljubav (trazim osobu)';
+const result = 'Pronasao jedan rezultat: Marija Zimska <mzimska@ffos.hr>';
+const searching = '.';
+const numberOfRepetitions = 7;
+
+const calculate = () => {
+    console.log(calculating);
+    for (let i = 0; i < numberOfRepetitions; i++) {
+        console.log(searching);
+    }
+    console.log(result);
+}
